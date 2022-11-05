5bf11992d4bed8a0355ed0e4d88fb3c6ac539eb3
gloria <gloriacheung812@gmail.com>
2022-11-05 09:32:54 -0400

    import quizbrain class call start_game to start

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 # create question bank which is list of objects from Question class
 question_bank = []
 for row in question_data:
     question = Question(row["text"], row["answer"])
     question_bank.append(question)
+
+quiz_brain = QuizBrain(question_bank)
+quiz_brain.start_game()
